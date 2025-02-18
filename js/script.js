5c0a6e25f7c390f59dc6cba8ca34bcb4143534ba
Zacky Raechan <muhamadraechanulwanzacky@Jake.local>
2025-02-18 11:57:38 +0800

    Fixing Bugs

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function openSettings(event) {
+    let settingsMenu = document.getElementById("settings");
+
+    // Toggle visibilitas
+    if (settingsMenu.style.display === "flex") {
+        settingsMenu.style.display = "none";
+    } else {
+        settingsMenu.style.display = "flex";
+    }
+
+    // Biar klik di luar menu nutup settings
+    document.addEventListener("click", function closeMenu(e) {
+        if (!settingsMenu.contains(e.target) && !event.target.closest("button")) {
+            settingsMenu.style.display = "none";
+            document.removeEventListener("click", closeMenu);
+        }
+    });
+}
+
+function openModal() {
+    document.getElementById("modal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
